640e5bdc3be2459f1c01f167b20c832b358fd938
DoubleGio <giorgiegoiati@gmail.com>
2021-09-27 14:16:11 +0200

    plots!

--- plot.py
+++ plot.py
@@ -1,15 +1,23 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-ENEMY = 1
+EA = 2
+ENEMY = 3
 N_GENS = 25
-N_TESTS = 5
+N_TESTS = 10
+
 best = np.zeros((0, 25))
 means = np.zeros((0, 25))
 for i in range(N_TESTS):
-    file = np.loadtxt(f'../enemy{ENEMY}_test{i+1}_gio/results.txt', skiprows=3, max_rows=25)
-    best = np.vstack((best, file[:, 1]))
-    means = np.vstack((means, file[:, 2]))
+    if EA == 2:
+        file = np.loadtxt(f'./enemy{ENEMY}_test{i+1}/results.txt', skiprows=1, max_rows=N_GENS)
+        best = np.vstack((best, file[:, 1]))
+        means = np.vstack((means, file[:, 2]))
+    else:
+        file = np.loadtxt(f'./Pygad_Chan_enemy{ENEMY}_experiment{i+1}/results.txt', skiprows=1, max_rows=N_GENS)
+        best = np.vstack((best, file[:, 3]))
+        means = np.vstack((means, file[:, 1]))
+
 
 best_mean = np.mean(best, axis=0)
 best_std = np.std(best, axis=0)
@@ -23,7 +31,10 @@ plt.fill_between(range(25), best_mean-best_std, best_mean+best_std, color='tab:o
 plt.plot(means_mean, label='mean of means', color='tab:blue', marker='o')
 plt.fill_between(range(25), means_mean-means_std, means_mean+means_std, color='tab:blue', alpha=0.5)
 
+plt.title(f'Enemy #{ENEMY} - test results')
+plt.ylim(-10, 100)
 plt.xlabel('Run #')
 plt.ylabel('Fitness')
 plt.legend()
+plt.savefig(f'./plots/EA{EA} test results - Enemy #{ENEMY}')
 plt.show()
