eb63c3c359d791171ab15fc9116a115b1b38806d
DoubleGio <giorgiegoiati@gmail.com>
2021-10-07 23:25:05 +0200

    Update plot_general.py

--- plot_general.py
+++ plot_general.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 N_GENS = 25
-N_TESTS = 2
+N_TESTS = 4
 ENEMIES = [2, 4, 6, 8]
 FOLDER = "EAGio results"
 
@@ -22,7 +22,7 @@ best_std = np.std(best, axis=0)
 means_mean = np.mean(means, axis=0)
 means_std = np.std(means, axis=0)
 
-plt.title(f'EA Adaptive - Results {ENEMIES}')
+plt.title(f'EA Adaptive - Enemies {ENEMIES}')
 plt.xlabel('Generation #')
 plt.ylabel('Fitness')
 plt.ylim(-10, 100)
@@ -32,7 +32,6 @@ plt.fill_between(range(N_GENS), best_mean - best_std, best_mean + best_std, colo
 plt.plot(means_mean, label='mean of means', color='tab:blue', marker='o')
 plt.fill_between(range(N_GENS), means_mean - means_std, means_mean + means_std, color='tab:blue', alpha=0.5)
 plt.legend()
-plt.show()
 
 plt.savefig(f'plots/{FOLDER} lineplots.png')
 plt.show()
