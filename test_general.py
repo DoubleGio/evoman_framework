f5c99ce68374fa500e1f0fdc0d3724029dd20df0
DoubleGio <giorgiegoiati@gmail.com>
2021-10-14 15:40:39 +0200

    tests

--- test_general.py
+++ test_general.py
@@ -1,4 +1,4 @@
-# Creating the boxplots and table
+# Testing the solutions and creating the gain boxplots and table
 # VU - Evolutionary Algorithms - team 49
 # September 2021
 import sys
@@ -8,8 +8,9 @@ from environment import Environment
 from demo_controller import player_controller
 import os
 from matplotlib import pyplot as plt
+from tabulate import tabulate
+from scipy import stats
 import numpy as np
-
 
 N_RUNS = 10
 N_REPEATS = 5
@@ -24,44 +25,75 @@ if HEADLESS:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
+def main():
+    best_sol = np.zeros(265)
+    best_gain = -800  # -800 is the worst possible performance (800 is the best)
+    best_results = np.zeros((10, 2))
+    data = {'EA Det. - Odd': [], 'EA Det. - Even': [], 'EA Ada. - Odd': [], 'EA Ada. - Even': []}
+    for i, key in enumerate(data):
+        gains = np.zeros(10)
+        for run in range(N_RUNS):
+            experiment_loc = f'{FOLDERS[i]}_test{run + 1}'
+            run_best_sol = np.loadtxt(experiment_loc + '/best.txt')
+            res_run = np.zeros((len(ENEMIES), 2))  # Columns: Player life, Enemy life
+            for repeat in range(N_REPEATS):
+                print(f'\n{FOLDERS[i]} - RUN {run + 1} - REPEAT {repeat + 1}\n')
+                env = Environment(experiment_name=experiment_loc,
+                                  enemies=ENEMIES,
+                                  playermode="ai",
+                                  player_controller=player_controller(N_NEURONS),
+                                  enemymode="static",
+                                  level=2,
+                                  speed="fastest",
+                                  randomini="yes",
+                                  multiplemode="yes")
+                env.cons_multi = custom_cons_multi
+                _, player_life, enemy_life, _ = env.play(pcont=run_best_sol)
+                res_run[:, 0] += player_life
+                res_run[:, 1] += enemy_life
+            res_run = res_run / N_REPEATS  # Average results of all enemies for 1 run
+
+            gains[run] = np.sum(res_run[:, 0]) - np.sum(res_run[:, 1])
+            if gains[run] > best_gain:
+                best_gain = gains[run]
+                best_sol = run_best_sol
+                best_results = res_run
+        data[key] = gains
+    np.savetxt('best_solution.text', best_sol)
+    plot(data)
+    ttest_odd = stats.ttest_ind(data['EA Det. - Odd'], data['EA Ada. - Odd'])
+    ttest_even = stats.ttest_ind(data['EA Det. - Even'], data['EA Ada. - Even'])
+    with open('table.txt', "w") as file:
+        table = tabulate(best_results, showindex=ENEMIES, headers=['Enemy #', 'Player life', 'Enemy life'],
+                         tablefmt='presto')
+        print("\n BEST RESULTS: \n")
+        print(table)
+        print(f'\n BEST GAIN: {best_gain}')
+        print(f'\n TTest Det. vs Ada. algorithm - ODD enemies, p-value = {ttest_odd.pvalue}')
+        print(f'\n TTest Det. vs Ada. algorithm - EVEN enemies, p-value = {ttest_even.pvalue}')
+        file.write(table)
+        file.write(f'\n TTest Det. vs Ada. algorithm - ODD enemies, p-value = {ttest_odd.pvalue}')
+        file.write(f'\n TTest Det. vs Ada. algorithm - EVEN enemies, p-value = {ttest_even.pvalue}')
+
+
+def plot(data):
+    fig, ax = plt.subplots()
+    median_props = dict(color='black')
+    box = ax.boxplot(data.values(), patch_artist=True, labels=data.keys(), medianprops=median_props)
+    colours = ['royalblue', 'lightsteelblue', 'sandybrown', 'peachpuff']
+    for patch, colour in zip(box['boxes'], colours):
+        patch.set_facecolor(colour)
+
+    ax.set_title(f'5 Tests vs. All Enemies')
+    ax.set_ylabel('Gain')
+
+    plt.savefig(f'plots/boxplots.png')
+    plt.show()
+
+
 def custom_cons_multi(values):
-    return values.mean()
+    return values
 
 
-data = {'EA Det. - Odd': [], 'EA Det. - Even': [], 'EA Ada. - Odd': [], 'EA Ada. - Even': []}
-for i, key in enumerate(data):
-    r = []
-    for run in range(N_RUNS):
-        experiment_loc = f'{FOLDERS[i]}_test{run + 1}'
-        f = []
-        for repeat in range(N_REPEATS):
-            env = Environment(experiment_name=experiment_loc,
-                              enemies=ENEMIES,
-                              playermode="ai",
-                              player_controller=player_controller(N_NEURONS),
-                              enemymode="static",
-                              level=2,
-                              speed="fastest",
-                              randomini="yes",
-                              multiplemode="yes")
-            env.cons_multi = custom_cons_multi
-            best_sol = np.loadtxt(experiment_loc + '/best.txt')
-            print(f'\n{FOLDERS[i]} - RUN {run + 1} - REPEAT {repeat + 1}\n')
-            fitness, player_life, enemy_life, _ = env.play(pcont=best_sol)
-            f.append(fitness)
-        r.append(np.mean(f))
-    data[key] = r
-
-fig, ax = plt.subplots()
-medianprops = dict(color='black')
-box = ax.boxplot(data.values(), patch_artist=True, labels=data.keys(), medianprops=medianprops)
-colours = ['royalblue', 'lightsteelblue', 'sandybrown', 'peachpuff']
-for patch, colour in zip(box['boxes'], colours):
-    patch.set_facecolor(colour)
-
-ax.set_title(f'5 Tests vs. All Enemies')
-ax.set_ylabel('Fitness')
-ax.set_ylim(0, 100)
-
-plt.savefig(f'plots/boxplots.png')
-plt.show()
+if __name__ == "__main__":
+    main()
